e99f98d77dff69e1af12152996590543b96d33ee
Chris G <stefan_gruescu@yahoo.com>
2021-03-24 15:08:49 +0200

    some refactor

--- src/shared/components/AnimatedList/UI/ExpandedListItem/ExpandedListItem.js
+++ src/shared/components/AnimatedList/UI/ExpandedListItem/ExpandedListItem.js
@@ -6,18 +6,14 @@ import PropTypes from 'prop-types';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import LocationCityIcon from '@material-ui/icons/LocationCity';
 import AddBoxIcon from '@material-ui/icons/AddBox';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import CardActionArea from '@material-ui/core/CardActionArea';
 
 import ImageGallery from '@/shared/components/ImageGallery';
 
 import Sections from '../Section/Section';
 import {StyledImage, ExtendedDescription} from '../ListItem/ListItem.styled';
+import FreeTextCard from '../FreeTextCard/FreeTextCard';
 
-import {StyledExpandedListItem, ExpandedListItemContent, ExpandedAvatar, AdditionalContent, AnimatedInFlipped, StyledCardImage, StyledImageContainer, StyledFreeTextArea, SectionsWrapper} from './ExpandedListItem.styled';
+import {StyledExpandedListItem, ExpandedListItemContent, ExpandedAvatar, AdditionalContent, AnimatedInFlipped, StyledCardImage, StyledImageContainer, SectionsWrapper} from './ExpandedListItem.styled';
 
 const ExpandedListItem = ({index, onClick, createCardFlipId, listData}) => {
   const propArray = [{
@@ -62,12 +58,9 @@ const ExpandedListItem = ({index, onClick, createCardFlipId, listData}) => {
       stagger="card"
     >
       <StyledExpandedListItem>
-
         <Flipped inverseFlipId={createCardFlipId(index)}>
           <div>
-
             <ExpandedListItemContent onClick={() => onClick(index)}>
-
               <Flipped
                 flipId={`avatar-${index}`}
                 stagger="card-content"
@@ -85,9 +78,7 @@ const ExpandedListItem = ({index, onClick, createCardFlipId, listData}) => {
                 <RenderSections />
               </SectionsWrapper>
             </ExtendedDescription>
-
             <AdditionalContent>
-
               {listData.images?.length && (
                 <>
                   <StyledImageContainer>
@@ -97,41 +88,15 @@ const ExpandedListItem = ({index, onClick, createCardFlipId, listData}) => {
                   </StyledImageContainer>
 
                 </>
-
               )}
               {listData.freeText ? (
-                <Card style={{maxWidth: '90%', maxHeight: '100%', marginLeft: '5%', borderRadius: '12.5px', height: '300px'}}>
-                  <CardActionArea style={{backgroundColor: '#444'}}>
-                    <div style={{display: 'flex', flexDirection: 'row', height: '300px'}}>
-                      <CardMedia
-                        style={{width: '250px'}}
-                        component="img"
-                        alt="Contemplative Reptile"
-                        width="250"
-                        height="300"
-                        image={listData.images[0]}
-                        title="Contemplative Reptile"
-                      />
-
-                      <CardContent style={{color: 'white'}}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          Description
-                        </Typography>
-                        <Typography variant="body2" color="white" component="p">
-                          {listData.freeText}
-                        </Typography>
-                      </CardContent>
-                    </div>
-                  </CardActionArea>
-                </Card>
+                <FreeTextCard image={listData.images[0]} text={listData.freeText} />
               ) : null }
             </AdditionalContent>
           </div>
         </Flipped>
-
       </StyledExpandedListItem>
     </AnimatedInFlipped>
-
   );
 };
 ExpandedListItem.propTypes = {
