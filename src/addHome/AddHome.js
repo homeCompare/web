8b327f2dc31bf1054e8fd492f9671b74aeb8c016
Chris G <stefan_gruescu@yahoo.com>
2021-05-30 16:28:05 +0300

    fixing fetch methods

--- src/addHome/AddHome.js
+++ src/addHome/AddHome.js
@@ -1,19 +1,19 @@
-import React, {memo, useState} from 'react';
+import React, { memo, useState } from 'react';
 
-import {useDispatch, useSelector} from 'react-redux';
-import {omit} from 'lodash';
-import {v4 as uuidv4} from 'uuid';
-import {useRouter} from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { omit } from 'lodash';
+import { v4 as uuidv4 } from 'uuid';
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
 import CustomSwitch from '@/shared/components/CustomSwitch/CustomSwitch';
 import Switch from '@/shared/components/Switch';
-import {toBase64} from '@/shared/utils/base64';
-import {upsertHomeToDb} from '@/shared/utils/db';
+import { toBase64 } from '@/shared/utils/base64';
+import { upsertHomeToDb } from '@/shared/utils/db';
 import * as actions from '@/state/actions';
 import HomeForm from '@/shared/components/HomeForm';
-import {buyFields, rentFields} from '@/shared/utils/homeFields';
-import {event} from '@/shared/utils/gtag';
+import { buyFields, rentFields } from '@/shared/utils/homeFields';
+import { event } from '@/shared/utils/gtag';
 
 const StyledSwitch = styled(Switch)`
   &&& {
@@ -25,47 +25,70 @@ const StyledSwitch = styled(Switch)`
 
 const AddHome = () => {
   const dispatch = useDispatch();
-  const user = useSelector(state => state.user.data);
+  const user = useSelector((state) => state.user.data);
   const router = useRouter();
-  const tempImages = useSelector(state => state.dropzone);
+  const tempImages = useSelector((state) => state.dropzone);
   const [isRent, setIsRent] = useState(false);
   const fields = isRent ? rentFields : buyFields;
 
   const onSubmit = async (formValues = {}) => {
     // modifying dropImage into images (array of strings of base64).
-    const modefiedFormValues = formValues.dropImage ? {
-      ...omit(formValues, 'dropImage'),
-      images: await Promise.all(tempImages.map(await toBase64)),
-    } : formValues;
+    const modefiedFormValues = formValues.dropImage
+      ? {
+          ...omit(formValues, 'dropImage'),
+          images: await Promise.all(tempImages.map(await toBase64)),
+        }
+      : formValues;
 
     modefiedFormValues.id = uuidv4();
     if (isRent) modefiedFormValues.type = 'rent';
     else modefiedFormValues.type = 'buy';
     modefiedFormValues.price = Number(modefiedFormValues.price);
 
-    event('new_home_added', 'home_action', 'key_address', [formValues?.city, formValues?.street, formValues?.houseNumber].join(', '));
+    event(
+      'new_home_added',
+      'home_action',
+      'key_address',
+      [formValues?.city, formValues?.street, formValues?.houseNumber].join(', ')
+    );
 
     await dispatch(actions.addHome(modefiedFormValues));
 
-      // if user is premium call upserthometodb
+    // if user is premium call upserthometodb
     if (user) {
-      if (user.isPremium) await upsertHomeToDb(user.id, modefiedFormValues, undefined);
+      if (user.isPremium) await upsertHomeToDb(modefiedFormValues, undefined);
     }
     await dispatch(actions.removeAllTempImages());
     router.push(`/home/${modefiedFormValues.id}`);
   };
   // temp
-  const initialValues = fields.filter(({name}) => name !== 'dropImage')
-    .reduce((all, item) => ({
-      ...(all || {}),
-      [item.name]: item.initialValue,
-    }), {});
+  const initialValues = fields
+    .filter(({ name }) => name !== 'dropImage')
+    .reduce(
+      (all, item) => ({
+        ...(all || {}),
+        [item.name]: item.initialValue,
+      }),
+      {}
+    );
 
   // temp Add shouldn't have initialValues on production.
   return (
     <>
-      <div style={{display: 'flex', alignItems: 'center', textAlign: 'center', justifyContent: 'center'}}>
-        <CustomSwitch onChange={() => { setIsRent(!isRent); dispatch(actions.removeAllTempImages()); }} />
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          textAlign: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <CustomSwitch
+          onChange={() => {
+            setIsRent(!isRent);
+            dispatch(actions.removeAllTempImages());
+          }}
+        />
       </div>
       <HomeForm
         onSubmit={onSubmit}
--- src/state/actions.js
+++ src/state/actions.js
@@ -1,22 +1,29 @@
-import {makeActionCreator} from 'redux-toolbelt';
-import {makeThunkAsyncActionCreator} from 'redux-toolbelt-thunk';
+import { makeActionCreator } from 'redux-toolbelt';
+import { makeThunkAsyncActionCreator } from 'redux-toolbelt-thunk';
 import axios from 'axios';
 
-import {facebookLogIn} from '@/shared/utils/auth';
+import { facebookLogIn } from '@/shared/utils/auth';
 
-export const facebookLogin = makeThunkAsyncActionCreator('FACEBOOK_LOGIN', async (type, homesList) => {
-  try {
-    const userData = await facebookLogIn();
+export const facebookLogin = makeThunkAsyncActionCreator(
+  'FACEBOOK_LOGIN',
+  async (type, homesList) => {
+    try {
+      const userData = await facebookLogIn();
 
-    const response = await axios.post('/api/login', {user: userData, homes: homesList, type}, {withCredentials: true});
+      const response = await axios.post(
+        '/api/login',
+        { user: userData, homes: homesList, type },
+        { withCredentials: true }
+      );
 
-    if (response) return await response.data.response;
-    return [];
-  } catch (error) {
-    console.error('facebookLogin failed', {error});
-    return error;
+      if (response) return await response.data.response;
+      return [];
+    } catch (error) {
+      console.error('facebookLogin failed', { error });
+      return error;
+    }
   }
-});
+);
 
 export const logout = makeActionCreator('LOGOUT', () => null);
 
@@ -26,15 +33,20 @@ export const addHome = makeActionCreator('ADD_HOME');
 export const editHome = makeActionCreator('EDIT_HOME');
 export const removeHomeById = makeActionCreator('REMOVE_HOME_BY_ID');
 export const setCurrency = makeActionCreator('SET_CURRENCY');
-export const getHomesFromDb = makeThunkAsyncActionCreator('GET_HOMES_FROM_DB', async (userId) => {
-  try {
-    const response = await axios.post('/api/gethomes', {userId});
-    return response.data.homes;
-  } catch (error) {
-    console.error('facebookLogin failed', {error});
-    return error;
+export const getHomesFromDb = makeThunkAsyncActionCreator(
+  'GET_HOMES_FROM_DB',
+  async () => {
+    try {
+      const response = await axios.post('/api/gethomes', {
+        withCredentials: true,
+      });
+      return response.data.homes;
+    } catch (error) {
+      console.error('facebookLogin failed', { error });
+      return error;
+    }
   }
-});
+);
 
 // DROPZONE ACTIONS
 export const addTempImages = makeActionCreator('ADD_TEMP_IMAGES');
